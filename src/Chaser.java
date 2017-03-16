0b69e3432813e3f34042d89f14a0187f07200500
ryan_kim <rwkim77@gmail.com>
2017-03-16 08:48:24 -0400

    Push

---
+++ src/Chaser.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+/**
+ * Created by ryan_kim on 4/16/17
+ */
+public class Chaser extends Sprite {
+
+    private Sprite target;
+
+    public Chaser(int x, int y, World world) {
+        super(x, y, EAST, world);
+
+        ArrayList<Sprite> sprites = world.getAllSprites();
+        target = sprites.get( (int)(Math.random()*sprites.size()) );
+        while(target.equals(this) && sprites.size() > 1){
+            target = sprites.get( (int)(Math.random()*sprites.size()) );
+        }
+        setPic("scorpion.png", NORTH);
+        setSpeed(10);
+    }
+
+    @Override
+    public void update(){
+        int d = getWorld().getDirection(this.getLoc(), target.getLoc());
+        setDir(d);
+
+        super.update();
+    }
+
+    public Sprite getTarget(){
+        return target;
+    }
+    public void setTarget(Sprite newTarget){
+        target = newTarget;
+    }
+
+    public void pickTarget(){
+        ArrayList<Sprite> sprites = getWorld().getAllSprites();
+        target = sprites.get( (int)(Math.random()*sprites.size()) );
+        while(target.equals(this) && sprites.size() > 1){
+            target = sprites.get( (int)(Math.random()*sprites.size()) );
+        }
+    }
+
+}
--- src/GameMain.java
+++ src/GameMain.java
@@ -1,10 +1,123 @@
-/**
- * Created by ryan_kim on 3/15/17.
- */
-public class GameMain {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class GameMain extends JPanel {
+
+    public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
+
+    private Timer timer;
+    private World theWorld;
+
+    public GameMain(){
+
+        theWorld = new World(FRAMEWIDTH, FRAMEHEIGHT);
+
+        //These are the Sprites that are added to the World...
+        for (int i = 0; i < 1; i++) {
+            int rand = (int)(Math.random()*3);
+            int x = (int)(Math.random()*400 + 50);
+            int y = (int)(Math.random()*400 + 50);
+//            if(rand == 0)
+//                theWorld.addSprite(new Bug(x, y, theWorld));
+//            else if(rand == 1)
+//                theWorld.addSprite(new BoxSprite(x, y, (int)(Math.random()*40+15), theWorld));
+//            else if(rand == 2 && theWorld.getAllSprites().size() > 1)
+//                theWorld.addSprite(new BoxSprite(x, y, 30, theWorld));
+//            else
+//                theWorld.addSprite(new Sprite(x, y, Sprite.EAST, theWorld));
+        }
+//        theWorld.addSprite(new Chaser(10, 10, theWorld));
+
+
+        timer = new Timer(40, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                //This will call update on each sprite.
+                theWorld.updateSprites();
+                repaint();
+            }
+        });
+        timer.start();
+
+
+        //EventListeners.  Not using them at the moment.
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+
+            }
+        });
+
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+                //Ask the world if any sprites contain the click
+                theWorld.click(mouseEvent);
+
+                repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+
+            }
+        });
+
+
+    }
+
+    //Our paint method.
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        //Draws all the sprites.
+        theWorld.drawSprites(g2);
+
+    }
+
+    //sets ups the panel and frame.
     public static void main(String[] args) {
+        JFrame window = new JFrame("SpriteLand");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 20 due to title bar.
 
-        //Hey Chris
+        Main panel = new Main();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
     }
 }
